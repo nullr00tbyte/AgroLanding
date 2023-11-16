5d90c1a85d45d66936cbaabf3b5e9739c59f65af
Josue Ramos <nullrootbyte@gmail.com>
2023-11-16 10:50:51 -0600

    analytics 2 vercel dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
